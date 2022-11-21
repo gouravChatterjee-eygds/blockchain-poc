8173bad883c92b9feea4bd2a299abb29060c2c65
Gourav Chatterjee <gourav.chatterjee1@gds.ey.com>
2022-11-21 12:37:49 +0530

    controllers

--- src/controllers/Asset.controller.js
+++ src/controllers/Asset.controller.js
@@ -5,127 +5,124 @@ const { Contract, providers } = require("ethers");
 // Create the provide of localhost
 const httpProvider = new providers.JsonRpcProvider("http://127.0.0.1:8545/");
 
-const assetContract = (address) => {
-  return new Contract(
-    AssetContractAddress.Asset,
-    AssetContractDetails.abi,
-    httpProvider.getSigner(address)
-  );
-};
-
-async function createAsset(req) {
-  try {
-    const assetContractDetails = assetContract(req.body.address);
-    const transaction = await assetContractDetails.mintToken(
-      req.body.name,
-      req.body.desc,
-      req.body.time
+class AssetController {
+  assetContract = (address) => {
+    return new Contract(
+      AssetContractAddress.Asset,
+      AssetContractDetails.abi,
+      httpProvider.getSigner(address)
     );
-    await httpProvider.waitForTransaction(transaction.hash);
-    const trResponse = await httpProvider.getTransactionReceipt(
-      transaction.hash
-    );
-    // console.log(trResponse);
-    const tokenId = parseInt(trResponse.logs[0].topics[3], 16);
-    return { statusCode: 200, tokenId: tokenId };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 502 };
+  };
+
+  async createAsset(req) {
+    try {
+      const assetContractDetails = this.assetContract(req.body.address);
+      const transaction = await assetContractDetails.mintToken(
+        req.body.name,
+        req.body.desc,
+        req.body.time
+      );
+      await httpProvider.waitForTransaction(transaction.hash);
+      const trResponse = await httpProvider.getTransactionReceipt(
+        transaction.hash
+      );
+      // console.log(trResponse);
+      const tokenId = parseInt(trResponse.logs[0].topics[3], 16);
+      return { statusCode: 200, tokenId: tokenId };
+    } catch (error) {
+      console.log(error);
+      return { statusCode: 502 };
+    }
+  }
+
+  async getListedAssetList() {
+    try {
+      const assetContractDetails = this.assetContract(
+        AssetContractAddress.Asset
+      );
+      let getAssetList = await assetContractDetails.assetList();
+      getAssetList = getAssetList.filter((asset) => asset.name !== "");
+      let assetList = getAssetList.map((asset) => {
+        return {
+          id: asset.id.toNumber(),
+          name: asset.name,
+          desc: asset.desc,
+          listed: asset.listed,
+          price: asset.price.toNumber(),
+        };
+      });
+      return { statusCode: 200, assetList: assetList };
+    } catch (error) {
+      console.log(error);
+      return { statusCode: 502 };
+    }
+  }
+
+  async getAssetListofUser(req) {
+    try {
+      const assetContractDetails = this.assetContract(req.body.address);
+      let getAssetList = await assetContractDetails.assetList();
+      getAssetList = getAssetList.filter((asset) => asset.name !== "");
+      let assetList = getAssetList.map((asset) => {
+        return {
+          id: asset.id.toNumber(),
+          name: asset.name,
+          desc: asset.desc,
+          listed: asset.listed,
+          price: asset.price.toNumber(),
+        };
+      });
+      return { statusCode: 200, assetList: assetList };
+    } catch (error) {
+      console.log(error);
+      return { statusCode: 502 };
+    }
+  }
+
+  async listAsset(req) {
+    try {
+      let assetContractDetails = this.assetContract(req.body.address);
+      await assetContractDetails.listAsset(req.body.tokenId, req.body.price);
+      return { readyForSell: true, statusCode: 200 };
+    } catch (error) {
+      console.log(error);
+      return { statusCode: 502 };
+    }
+  }
+
+  async buyAsset(req) {
+    try {
+      let assetContractDetails = this.assetContract(req.body.address);
+      const transaction = await assetContractDetails.buyAsset(
+        req.body.tokenId,
+        req.body.time
+      );
+
+      await httpProvider.waitForTransaction(transaction.hash);
+
+      return { statusCode: 200 };
+    } catch (err) {
+      console.error(`NFTMarketContract: `, err.message);
+      return { statusCode: 502 };
+    }
+  }
+
+  async getAssetOwnershipHistory(req) {
+    try {
+      const assetContractDetails = this.assetContract(req.body.address);
+
+      let assetHistory = await assetContractDetails.getAssetHistory(
+        req.body.tokenId
+      );
+      assetHistory = assetHistory.map((asset) => {
+        return { owner: asset.owner, time: asset.time.toNumber() };
+      });
+      return { statusCode: 200, assetHistory: assetHistory };
+    } catch (error) {
+      console.error(`assetContract: `, err.message);
+      return { statusCode: 502 };
+    }
   }
 }
 
-async function getListedAssetList() {
-  try {
-    const assetContractDetails = assetContract(AssetContractAddress.Asset);
-    let getAssetList = await assetContractDetails.assetList();
-    getAssetList = getAssetList.filter((asset) => asset.name !== "");
-    let assetList = getAssetList.map((asset) => {
-      return {
-        id: asset.id.toNumber(),
-        name: asset.name,
-        desc: asset.desc,
-        listed: asset.listed,
-        price: asset.price.toNumber(),
-      };
-    });
-    return { statusCode: 200, assetList: assetList };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 502 };
-  }
-}
-
-async function getAssetListofUser(req) {
-  try {
-    const assetContractDetails = assetContract(req.body.address);
-    let getAssetList = await assetContractDetails.assetList();
-    getAssetList = getAssetList.filter((asset) => asset.name !== "");
-    let assetList = getAssetList.map((asset) => {
-      return {
-        id: asset.id.toNumber(),
-        name: asset.name,
-        desc: asset.desc,
-        listed: asset.listed,
-        price: asset.price.toNumber(),
-      };
-    });
-    return { statusCode: 200, assetList: assetList };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 502 };
-  }
-}
-
-async function listAsset(req) {
-  try {
-    let assetContractDetails = assetContract(req.body.address);
-    await assetContractDetails.listAsset(req.body.tokenId, req.body.price);
-    return { readyForSell: true, statusCode: 200 };
-  } catch (error) {
-    console.log(error);
-    return { statusCode: 502 };
-  }
-}
-
-async function buyAsset(req) {
-  try {
-    let assetContractDetails = assetContract(req.body.address);
-    const transaction = await assetContractDetails.buyAsset(
-      req.body.tokenId,
-      req.body.time
-    );
-
-    await httpProvider.waitForTransaction(transaction.hash);
-
-    return { statusCode: 200 };
-  } catch (err) {
-    console.error(`NFTMarketContract: `, err.message);
-    return { statusCode: 502 };
-  }
-}
-
-async function getAssetOwnershipHistory(req) {
-  try {
-    const assetContractDetails = assetContract(req.body.address);
-
-    let assetHistory = await assetContractDetails.getAssetHistory(
-      req.body.tokenId
-    );
-    assetHistory = assetHistory.map((asset) => {
-      return { owner: asset.owner, time: asset.time.toNumber() };
-    });
-    return { statusCode: 200, assetHistory: assetHistory };
-  } catch (error) {
-    console.error(`assetContract: `, err.message);
-    return { statusCode: 502 };
-  }
-}
-
-module.exports = {
-  createAsset,
-  getListedAssetList,
-  getAssetListofUser,
-  listAsset,
-  buyAsset,
-  getAssetOwnershipHistory,
-};
+module.exports = new AssetController();
--- src/controllers/Token.controller.js
+++ src/controllers/Token.controller.js
@@ -2,67 +2,65 @@ const TokenContract = require("../contract-details/Token.json");
 const TokenContractAddress = require("../contract-details/contract-address.json");
 const { Contract, utils, providers } = require("ethers");
 
-function currencyContract(address) {
-  let httpProvider = new providers.JsonRpcProvider("http://127.0.0.1:8545/");
+class TokenController {
+  currencyContract(address) {
+    let httpProvider = new providers.JsonRpcProvider("http://127.0.0.1:8545/");
 
-  return new Contract(
-    TokenContractAddress.Token,
-    TokenContract.abi,
-    httpProvider.getSigner(address)
-  );
-}
+    return new Contract(
+      TokenContractAddress.Token,
+      TokenContract.abi,
+      httpProvider.getSigner(address)
+    );
+  }
 
-async function mintCurrency(req, res) {
-  try {
-    console.log(req.body);
-    let tokenContract = currencyContract(req.body.address);
-    let transaction = await tokenContract.mintToken(req.body.amount, {
-      value: utils.parseEther(req.body.amount).toString(),
-    });
-    // // console.log(transaction);
-    await transaction.wait();
-    return {
-      statusCode: 200,
-      // data: tokenContract,
-      message: `You have successfully minted ${req.body.amount} tokens`,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 502,
-      message: "Sorry please try again",
-    };
+  async mintCurrency(req, res) {
+    try {
+      console.log(req.body);
+      let tokenContract = this.currencyContract(req.body.address);
+      let transaction = await tokenContract.mintToken(req.body.amount, {
+        value: utils.parseEther(req.body.amount).toString(),
+      });
+      // // console.log(transaction);
+      await transaction.wait();
+      return {
+        statusCode: 200,
+        // data: tokenContract,
+        message: `You have successfully minted ${req.body.amount} tokens`,
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        statusCode: 502,
+        message: "Sorry please try again",
+      };
+    }
+  }
+
+  async transferCurrency(req, res) {
+    try {
+      const tokenContract = this.currencyContract(req.body.address);
+      await tokenContract.transfer(req.body.to, req.body.amount);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async getCurrencyBalance(req, res) {
+    try {
+      const tokenContract = this.currencyContract(req.body.address);
+      const balance = (
+        await tokenContract.balanceOf(req.body.address)
+      ).toString();
+      return { statusCode: 200, balance: balance };
+    } catch (error) {
+      console.log(error);
+      return {
+        statusCode: 502,
+      };
+    }
   }
 }
 
-async function transferCurrency(req, res) {
-  try {
-    const tokenContract = currencyContract(req.body.address);
-    await tokenContract.transfer(req.body.to, req.body.amount);
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-}
-
-async function getCurrencyBalance(req, res) {
-  try {
-    const tokenContract = currencyContract(req.body.address);
-    const balance = (
-      await tokenContract.balanceOf(req.body.address)
-    ).toString();
-    return { statusCode: 200, balance: balance };
-  } catch (error) {
-    console.log(error);
-    return {
-      statusCode: 502,
-    };
-  }
-}
-
-module.exports = {
-  mintCurrency,
-  getCurrencyBalance,
-  transferCurrency,
-};
+module.exports = new TokenController();
